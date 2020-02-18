43fda43e4278db9e268e926dc95ea8a3dc5628a3
Archis Gore <archis@polyverse.com>
2020-02-18 13:26:57 -0800

    Committing PHP7.2 docker file copied over

---
+++ php7.4/apache/publish-image.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+image="polyverse/polyscripted-wordpress"
+
+echo "$(date) Obtaining current git sha for tagging the docker image"
+headsha=$(git rev-parse --verify HEAD)
+
+
+docker build -t $image:debian-$headsha .
+docker tag $image:debian-$headsha $image:debian
+
+if [[ "$1" == "-p" ]]; then
+	docker push $image:debian-$headsha
+	docker push $image:debian
+fi
