68b399a29e6427e88c380f23b6efbd06290518e6
Archis Gore <archis@polyverse.com>
2020-03-11 09:40:41 -0700

    Updated upstream polyscripted PHP container

--- php7.4/apache/publish-image.sh
+++ php7.4/apache/publish-image.sh
@@ -8,8 +8,10 @@ headsha=$(git rev-parse --verify HEAD)
 
 docker build -t $image:debian-$headsha .
 docker tag $image:debian-$headsha $image:debian
+docker tag $image:debian-$headsha $image:latest
 
 if [[ "$1" == "-p" ]]; then
 	docker push $image:debian-$headsha
 	docker push $image:debian
+	docker push $image:latest
 fi
