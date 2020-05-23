5ea634161bb641a61b81fb9f690af2daab15944b
Blue Gaston <bbluegaston@gmail.com>
2020-05-23 16:46:23 -0700

    removed test

--- php7.4/apache/scramble.sh
+++ php7.4/apache/scramble.sh
@@ -15,7 +15,6 @@ if [[ "$MODE" == "polyscripted" || -f /polyscripted ]]; then
 	echo "Starting polyscripted WordPress"
 	cd $POLYSCRIPT_PATH
 	sed -i "/#mod_allow/a \define( 'DISALLOW_FILE_MODS', true );" /var/www/html/wp-config.php
-	cat ./build-scrambled.sh
 	./build-scrambled.sh
 	if [ -f scrambled.json ] && s_php tok-php-transformer.php -p /var/www/html --replace; then
 		echo "Polyscripting enabled."
