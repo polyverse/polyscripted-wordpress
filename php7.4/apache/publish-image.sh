3dc086f1aba56ca143b3102dc9e0b7acf17aa86b
Blue Gaston <bbluegaston@gmail.com>
2020-05-23 15:36:21 -0700

    revert to known working state

--- php7.4/apache/publish-image.sh
+++ php7.4/apache/publish-image.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
 image="polyverse/polyscripted-wordpress"
-name="polyscripted-wordpress"
-tag="7.4-apache"
-gpr="docker.pkg.github.com/"$image"/"$name":"$tag
+
 echo "$(date) Obtaining current git sha for tagging the docker image"
 headsha=$(git rev-parse --verify HEAD)
 
-echo "Building and tagging image"
-docker build -t $image:$tag-$headsha .
-docker tag $image:$tag-$headsha $image:$tag-latest
-docker tag $image:$tag-latest $gpr-latest
-docker tag $image:$tag-latest $gpr
 
-echo "Pushing images"
+docker build -t $image:debian-$headsha .
+docker tag $image:debian-$headsha $image:debian
+docker tag $image:debian-$headsha $image:latest
+
 if [[ "$1" == "-p" ]]; then
-	docker push $image:$tag-$headsha
-	docker push $image:$tag-latest
-	docker push $gpr
-	docker push $gpr-latest
+	docker push $image:debian-$headsha
+	docker push $image:debian
+	docker push $image:latest
 fi
