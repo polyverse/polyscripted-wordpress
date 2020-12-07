1871154f70bd545e220458f84a60a6f53d0ccacd
Blue Gaston <bbluegaston@gmail.com>
2020-12-06 20:33:02 -0800

    added v1 run-self-contained

---
+++ php7.4/apache/run-self-contained.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MODE=$1
+
+echo "Running under mode: $MODE"
+
+image="polyverse/polyscripted-wordpress"
+
+echo "$(date) Obtaining current git sha for tagging the docker image"
+headsha=$(git rev-parse --verify HEAD)
+
+docker run --name mysql-host -e MYSQL_ROOT_PASSWORD=qwerty -d mysql:5.7
+docker run --rm -e MODE=$MODE --name wordpress -v $PWD/wordpress:/wordpress  --link mysql-host:mysql -p 8000:80  $image:apache-7.4-$headsha
