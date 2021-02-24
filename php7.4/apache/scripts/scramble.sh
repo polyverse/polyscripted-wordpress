426f3beec7cbdbfbe7a93ca0ada06979ec577236
Archis Gore <archis@polyverse.com>
2021-02-23 16:28:44 -0800

    Preserve permissions when copying

--- php7.4/apache/scripts/scramble.sh
+++ php7.4/apache/scripts/scramble.sh
@@ -15,19 +15,20 @@ if [[ "$MODE" == "polyscripted" || -f /polyscripted ]]; then
 	    echo "Copying /wordpress to /var/www/html to be polyscripted in place..."
 	    echo "This will prevent changes from being saved back to /wordpress, but will protect"
 	    echo "against code injection attacks..."
-		cp -R /wordpress /var/www/temp
+		cp -Rp /wordpress /var/www/temp
 	fi
 
 	echo "Starting polyscripted WordPress"
 	sed -i "/#mod_allow/a \define( 'DISALLOW_FILE_MODS', true );" /var/www/html/wp-config.php
     	./build-scrambled.sh
 	if [ -f scrambled.json ] && s_php tok-php-transformer.php -p /var/www/temp --replace; then
+		rm -rf /var/www/html
 		mv /var/www/temp /var/www/html
 		echo "Polyscripting enabled."
 		echo "done"
 	else
 		echo "Polyscripting failed."
-		cp /usr/local/bin/s_php /usr/local/bin/php
+		cp -p /usr/local/bin/s_php /usr/local/bin/php
 		exit 1
 	fi
 
@@ -41,8 +42,6 @@ else
     echo "Polyscripted mode is off. To enable it, either:"
     echo "  1. Set the environment variable: MODE=polyscripted"
     echo "  2. OR create a file at path: /polyscripted"
-
-
 
     if [ -d vanilla-save ]; then
 	    reset.sh
