335836f23faeb6294c79ef3bd386c4a53ddccfeb
Blue Gaston <bbluegaston@gmail.com>
2021-03-01 08:57:33 -0700

    Alpine php fpm not working

--- php7.2/fpm-alpine/publish-image.sh
+++ php7.2/fpm-alpine/publish-image.sh
@@ -9,7 +9,7 @@ headsha=$(git rev-parse --verify HEAD)
 echo "Copying scripts into current directory for docker build context..."
 cp -Rp ../../scripts .
 
-docker build -t $image:alpine-7.2-$headsha . --no-cache
+docker build -t $image:alpine-7.2-$headsha .
 docker tag $image:alpine-7.2-$headsha $image:alpine-7.2
 docker tag $image:alpine-7.2-$headsha $image:latest
 
@@ -17,6 +17,11 @@ if [[ "$1" == "-p" ]]; then
 	docker push $image:alpine-7.2-$headsha
 	docker push $image:alpine-7.2-latest
 	docker push $image:latest
+fi
+if [[ "$1" == "-g" ]]; then
+        echo "Pushing to Github Container Repository"
+        docker tag $image:alpine-7.2-$headsha ghcr.io/$image:alpine-7.2-$headsha
+        docker push ghcr.io/$image:alpine-7.2-$headsha
 fi
 
 echo "Removing temporary scripts"
--- php7.2/fpm-alpine/run-self-contained.sh
+++ php7.2/fpm-alpine/run-self-contained.sh
@@ -3,5 +3,36 @@ echo "Running under mode: $MODE"
 echo "$(date) Obtaining current git sha for tagging the docker image"
 headsha=$(git rev-parse --verify HEAD)
 
-echo "Starting wordpress with docker compose"
-headsha=$headsha MODE=$1 docker-compose up
+function fail {
+  echo $1 >&2
+  exit 1
+}
+
+function try_curl {
+  local n=1
+  local max=4
+  local delay=15
+  while true; do
+    curl http://localhost:8000/ && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        fail "Unable to connect to WordPress after $n attempts."
+      fi
+    }
+  done
+}
+
+if [[ "$1" == "-test" ]]; then
+        set -e
+	MODE=$mode headsha=$headsha docker compose up
+	sleep 55
+        try_curl
+        echo "Curled wordpress success. Testing polyscripted"
+        docker exec -t wordpress /bin/bash -c 'if [[ $(diff /wordpress/index.php /var/www/html/index.php) && ! $(php -l /wordpress/index.php) && $(php -l /var/www/html/index.php) ]]; then exit 0 else exit 1; fi'
+else
+	echo "Starting wordpress with docker compose"
+	MODE=$mode headsha=$headsha docker compose up
+fi
