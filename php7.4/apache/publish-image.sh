076fcd7a161a8c5a4e8f569c70ef9512e1732406
Blue Gaston <bbluegaston@gmail.com>
2021-03-05 10:07:05 -0700

    updated pubish image

--- php7.4/apache/publish-image.sh
+++ php7.4/apache/publish-image.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 image="polyverse/polyscripted-wordpress"
-
 echo "$(date) Obtaining current git sha for tagging the docker image"
 headsha=$(git rev-parse --verify HEAD)
 
 echo "Copying scripts into current directory for docker build context..."
 cp -Rp ../../scripts .
 
+#Build and Tag
 docker build -t $image:apache-7.4-$headsha .
 docker tag $image:apache-7.4-$headsha $image:apache-7.4
 docker tag $image:apache-7.4-$headsha $image:latest
 
+#Dockerhub Repository
 if [[ "$1" == "-p" ]]; then
 	docker push $image:apache-7.4-$headsha
 	docker push $image:apache-7.4
 	docker push $image:latest
 fi
-
+#Github Container Repository
 if [[ "$1" == "-g" ]]; then
         echo "Pushing to Github Container Repository"
         docker tag $image:apache-7.4-$headsha ghcr.io/$image:apache-7.4-$headsha
