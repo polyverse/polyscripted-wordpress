cc6a526accee05137aabeec83d09a8ef3e013f69
Archis <archis@polyverse.com>
2021-07-13 16:24:53 -0700

    Remove all 7.2 since it's no longer supported upstream

--- php7.2/apache/README.md
+++
@@ -1,3 +0,0 @@
-The Docker image for this is regularly updated at: 
-https://hub.docker.com/r/polyverse/polyscripted-wordpress/
-https://gchr.io/polyverse/polyscripted-wordpress
--- php7.2/fpm-alpine/run-self-contained.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-MODE=$1
-echo "Running under mode: $MODE"
-
-echo "$(date) Obtaining current git sha for tagging the docker image"
-headsha=$(git rev-parse --verify HEAD)
-
-echo "Starting wordpress with docker compose"
-MODE=$MODE headsha=$headsha docker-compose up
